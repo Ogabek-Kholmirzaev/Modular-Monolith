ad22fa3fad68a6c63609050c12697536b679caf4
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-23 22:19:26 +0500

    Aggregate

---
+++ src/Shared/Shared/DDD/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Shared.DDD;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvents(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] domainEvents = _domainEvents.ToArray();
+
+        _domainEvents.Clear();
+
+        return domainEvents;
+    }
+}
