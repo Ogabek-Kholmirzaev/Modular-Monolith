6de081e0d2dfcceba3f6f08bdd935ca9cbe67335
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-25 21:09:17 +0500

    Time Format Chnaged to UtcNow

--- src/Shared/Shared/DDD/IDomainEvent.cs
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -5,6 +5,6 @@ namespace Shared.DDD;
 public interface IDomainEvent : INotification
 {
     Guid EventId => Guid.NewGuid();
-    public DateTime OccuredOn => DateTime.Now;
+    public DateTime OccuredOn => DateTime.UtcNow;
     public string EventType => GetType().AssemblyQualifiedName!;
 }
