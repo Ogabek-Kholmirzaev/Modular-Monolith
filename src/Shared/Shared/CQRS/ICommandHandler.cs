0d48e1f8ea0ca094448561cb3a028d768ef31622
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-26 23:20:12 +0500

    Continued Abstraction on MediatR and CQRS...

---
+++ src/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
