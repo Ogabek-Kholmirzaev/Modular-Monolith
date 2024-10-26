d08148f665b90d7b91f37940778431e4f2e323fe
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-26 12:41:45 +0500

    Feature Handler with CQRS and MadiatR

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProductCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //TODO: need to implement business logic to create a new product
+        throw new NotImplementedException();
+    }
+}
