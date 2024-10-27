f896c0dcfecb318a3446e8664c77b37a0dd4684c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-27 21:30:38 +0500

    GetProductsHandler

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.Products.Features.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<ProductDto> Products);
+
+public class GetProductsHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .OrderBy(product => product.Name)
+            .ToListAsync(cancellationToken);
+
+        var productDtos = ProjectToProductsDto(products);
+
+        return new GetProductsResult(productDtos);
+    }
+
+    private IEnumerable<ProductDto> ProjectToProductsDto(List<Product> products)
+    {
+        throw new NotImplementedException();
+    }
+}
