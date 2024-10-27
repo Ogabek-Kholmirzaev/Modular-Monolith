5f8008e21be515ed8b7c45da161a9ec907126729
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-27 22:07:23 +0500

    GetProductByIdHandler

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.Products.Features.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(ProductDto Product);
+
+public class GetProductByIdHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await dbContext.Products
+            .AsNoTracking()
+            .SingleOrDefaultAsync(product => product.Id == query.Id, cancellationToken)
+            ?? throw new Exception($"Product not found: {query.Id}");
+
+        var productDto = product.Adapt<ProductDto>();
+
+        return new GetProductByIdResult(productDto);
+    }
+}
