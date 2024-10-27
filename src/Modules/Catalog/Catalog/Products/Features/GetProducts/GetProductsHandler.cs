c8a25f226c1cb0e694af3dc4f0861c7f4ebb6623
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-27 21:50:24 +0500

    GetProductByCategoryHandler

--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -7,7 +7,7 @@ public record GetProductsResult(IEnumerable<ProductDto> Products);
 public class GetProductsHandler(CatalogDbContext dbContext)
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
         var products = await dbContext.Products
             .AsNoTracking()
