452315f7bab17dae18144dbccfba51f8b4b90352
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-29 07:57:54 +0500

    CreateProductEndpoint POST with Carter

--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -13,3 +13,7 @@ global using Mapster;
 global using Catalog.Products.Events;
 global using MediatR;
 global using Microsoft.Extensions.Logging;
+global using Carter;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
