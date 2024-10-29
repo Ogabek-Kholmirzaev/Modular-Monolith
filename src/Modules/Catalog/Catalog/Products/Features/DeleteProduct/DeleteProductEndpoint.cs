6b1a474d0b242683648fcedd0bd0ce7e571de39d
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-29 08:58:40 +0500

    DeleteProductEndpoint DELETE

---
+++ src/Modules/Catalog/Catalog/Products/Features/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Products.Features.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+            var respone = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(respone);
+        })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
+}
