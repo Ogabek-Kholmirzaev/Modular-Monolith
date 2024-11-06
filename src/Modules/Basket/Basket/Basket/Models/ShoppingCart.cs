83e03b3afc7c75e7b27a47a8e9aa8b1cf1b3d369
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-06 19:17:40 +0500

    DDD Rich-Domain Model Entity for ShoppingCart

--- src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -7,4 +7,49 @@ public class ShoppingCart : Aggregate<Guid>
     public string UserName { get; private set; } = default!;
     public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public static ShoppingCart Create(Guid id, string userName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(userName);
+
+        var shoppingCart = new ShoppingCart
+        {
+            Id = id,
+            UserName = userName
+        };
+
+        return shoppingCart;
+    }
+
+    public void AddItem(
+        Guid productId,
+        int quantity,
+        string color,
+        decimal price,
+        string productName)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            _items.Add(new ShoppingCartItem(Id, productId, quantity, color, price, productName));
+        }
+    }
+
+    public void RemoveItem(Guid productId)
+    {
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            _items.Remove(existingItem);
+        }
+    }
 }
