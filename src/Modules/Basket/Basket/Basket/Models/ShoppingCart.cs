b8d03c89c0ec19f14e3a057366a5a55c7cfd6e9a
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-06 19:03:52 +0500

    ShoppingCart & ShoppingCartItem Models

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using Shared.DDD;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCart : Aggregate<Guid>
+{
+    private readonly List<ShoppingCartItem> _items = new();
+
+    public string UserName { get; private set; } = default!;
+    public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
