e6939684231da3d10db2ac44577ba671d6094f45
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-07 12:48:18 +0500

    Register BasketDbContext to DI

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Basket.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Basket;
 
@@ -9,6 +12,18 @@ public static class BasketModule
     public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
     {
         //add services to DI container
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<BasketDbContext>((serviceProvider, options) =>
+        {
+            var connectionString = configuration.GetConnectionString("Database")
+                ?? throw new ArgumentNullException("Database connection string is null");
+
+            options.UseNpgsql(connectionString);
+            options.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
+        });
 
         return services;
     }
