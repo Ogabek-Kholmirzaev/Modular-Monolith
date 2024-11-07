2ac41fb15c35208871a36c9cac1dddabbf0e68ee
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-07 12:40:27 +0500

    ShoppingCartItem Configuration

---
+++ src/Modules/Basket/Basket/Data/Configurations/ShoppingCartItemCofiguraiton.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Basket.Data.Configurations;
+
+public class ShoppingCartItemCofiguraiton : IEntityTypeConfiguration<ShoppingCartItem>
+{
+    public void Configure(EntityTypeBuilder<ShoppingCartItem> builder)
+    {
+        builder.HasKey(item  => item.Id);
+
+        builder.Property(item => item.ProductId)
+            .IsRequired();
+
+        builder.Property(item => item.Quantity)
+            .IsRequired();
+
+        builder.Property(item => item.Color)
+            .IsRequired();
+
+        builder.Property(item => item.Price)
+            .IsRequired();
+
+        builder.Property(item => item.ProductName)
+            .IsRequired();
+    }
+}
