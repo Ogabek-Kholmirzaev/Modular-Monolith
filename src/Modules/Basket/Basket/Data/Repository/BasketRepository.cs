de1751e9c5762fcc4911afcf76c3d9733d86e33f
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-10 21:29:41 +0500

    Basket Repository Implementation

---
+++ src/Modules/Basket/Basket/Data/Repository/BasketRepository.cs
@@ -0,0 +1,49 @@
+namespace Basket.Data.Repository;
+
+public class BasketRepository(BasketDbContext dbContext) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(
+        string userName,
+        bool asNoTracking = true,
+        CancellationToken cancellationToken = default)
+    {
+        var query = dbContext.ShoppingCarts
+            .Include(shoppingCart => shoppingCart.Items)
+            .Where(shoppingCart => shoppingCart.UserName == userName);
+
+        if (asNoTracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        var basket = await query.SingleOrDefaultAsync(cancellationToken) 
+            ?? throw new BasketNotFoundException(userName);
+
+        return basket;
+    }
+
+    public async Task<ShoppingCart> CreateBasketAsync(
+        ShoppingCart basket,
+        CancellationToken cancellationToken = default)
+    {
+        dbContext.Add(basket);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        var basket = await GetBasketAsync(userName, false, cancellationToken);
+
+        dbContext.ShoppingCarts.Remove(basket);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
