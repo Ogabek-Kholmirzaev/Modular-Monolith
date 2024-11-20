9c8aeb818ee1b69d2073ef10332ff617a32e4252
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-20 16:13:02 +0500

    RabbitMQ Configuration

--- src/Shared/Shared.Messaging/Extensions/MassTransitExtensions.cs
+++ src/Shared/Shared.Messaging/Extensions/MassTransitExtensions.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Shared.Messaging.Extensions;
@@ -8,6 +9,7 @@ public static class MassTransitExtensions
 {
     public static IServiceCollection AddMassTransitWithAssemblies(
         this IServiceCollection services,
+        IConfiguration configuration,
         params Assembly[] assemblies)
     {
         services.AddMassTransit(config =>
@@ -18,7 +20,17 @@ public static class MassTransitExtensions
             config.AddSagaStateMachines(assemblies);
             config.AddSagas(assemblies);
             config.AddActivities(assemblies);
-            config.UsingInMemory((context, configurator) => configurator.ConfigureEndpoints(context));
+            
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                
+                configurator.ConfigureEndpoints(context);
+            });
         });
 
         return services;
