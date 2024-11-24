5dfd170dedf71af04def9a7afc172da37e130a88
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-24 20:24:53 +0500

    CreateOrderEndpoint

--- src/Modules/Ordering/Ordering/GlobalUsings.cs
+++ src/Modules/Ordering/Ordering/GlobalUsings.cs
@@ -1,9 +1,13 @@
 // Global using directives
 
 global using System.Reflection;
+global using Carter;
 global using FluentValidation;
 global using Mapster;
 global using MediatR;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Logging;
