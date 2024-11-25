8e49763498f5d259ee3fc1df036c302b255355ba
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-25 22:36:46 +0500

    OutboxMessage DbSet & Migration to BasketDbContext

--- src/Modules/Basket/Basket/Basket/Models/OutboxMessage.cs
+++ src/Modules/Basket/Basket/Basket/Models/OutboxMessage.cs
@@ -1,6 +1,6 @@
 namespace Basket.Basket.Models;
 
-public class OutboxMessage
+public class OutboxMessage : Entity<Guid>
 {
     public string Type { get; set; } = default!;
     public string Content { get; set; } = default!;
